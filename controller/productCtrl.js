a310b48567de9a599f86c5269b6856af4c168b02
Nikhil Mudakavi <nikhil@rulezero.com>
2023-04-07 11:19:59 +0530

    product pagination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -23,9 +23,49 @@ const getAProduct = asyncHandler(async (req, res) => {
 });
 
 const getAllProduct = asyncHandler(async (req, res) => {
+ 
   try {
-    const products = await Product.find();
-    res.json(products);
+    // filtering
+    const queryObj = {...req.query};
+    const excludeFields = ["page","sort","limit","fields"];
+    excludeFields.forEach(element=> delete queryObj[element])
+    let queryStr = JSON.stringify(queryObj);
+    queryStr= queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match =>`$${match}`);
+   
+   let query = Product.find(JSON.parse(queryStr));
+    
+   //sorting
+   if(req.query.sort){
+    const sortBy = req.query.sort.split(",").join(" "); 
+    query = query.sort(sortBy);
+   }
+   else{
+    query = query.sort("-createdAt");
+   }
+
+   //limiting the feilds
+   if(req.query.fields){
+     const fields = req.query.fields.split(",").join(" "); 
+    query = query.select(fields);
+   }else{
+    query= query.select("--v");
+   }
+
+   //pagination
+   const page = req.query.page;
+   const limit = req.query.limit;
+   const skip = (page-1)* limit;
+   query= query.skip(skip).limit(limit);
+   if(req.query.page){
+    const productCount = await Product.countDocuments();
+    if(skip>= productCount) throw new Error("page doesn't exist");
+   }
+   console.log(page,limit,skip);
+   const product = await query;
+   // const {brand,color,category}=req.query;
+   // const products = await Product.find(queryObj);
+  // const products = await Product.where("category").equals(category);
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
